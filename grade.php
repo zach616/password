614d9c1b29ec8f54b769b99c60f140ec2447062c
zach616 <57763455+zach616@users.noreply.github.com>
2019-11-14 17:02:34 -0600

    Update grade.php

--- grade.php
+++ grade.php
@@ -1,1 +1,13 @@
 
+<?php
+    
+    $answer1 = $_POST['question-1-answers'];
+
+    $totalCorrect = 0;
+    
+    if ($answer1 == "wow") { $totalCorrect++; }
+    
+    
+    echo "<div id='results'>$totalCorrect / 1 correct</div>";
+    
+?>
